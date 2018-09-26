f60cece1c998228407aa830b98cec730dceb832b
aloaas <aloaas@gmail.com>
2018-09-26 20:02:26 +0300

    [Feat] #10 New victim button added

--- frontend/src/pages/overview.jsx
+++ frontend/src/pages/overview.jsx
@@ -1,70 +1,84 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import { TextField } from '@material-ui/core';
+import {withStyles} from '@material-ui/core/styles';
+import {TextField} from '@material-ui/core';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 
+
 import withRoot from '../withRoot';
 import Layout from '../components/layout';
-import { Link } from '@reach/router';
+import {Link} from '@reach/router';
 
 const styles = theme => ({
-  root: {
-    textAlign: 'center',
-    paddingTop: theme.spacing.unit * 20,
-  },
-  paper: {
-    marginTop: theme.spacing.unit * 4,
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme
-      .spacing.unit * 3}px`,
-  },
-  input: {
-    margin: theme.spacing.unit,
-  },
+    root: {
+        textAlign: 'center',
+        paddingTop: theme.spacing.unit * 20,
+    },
+    paper: {
+        marginTop: theme.spacing.unit * 4,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme
+            .spacing.unit * 3}px`,
+    },
+    input: {
+        margin: theme.spacing.unit,
+    },
 });
 
 class Overview extends React.Component {
-  static propTypes = {
-    classes: PropTypes.object.isRequired,
-  };
+    static propTypes = {
+        classes: PropTypes.object.isRequired,
+    };
 
-  state = {
-    id: '',
-  };
+    state = {
+        id: '',
+    };
 
-  render() {
-    const { classes } = this.props;
+    render() {
+        const {classes} = this.props;
 
-    return (
-      <Layout>
-        <Paper className={classes.paper}>
-          <TextField
-            id="id-field"
-            label="ID"
-            className={classes.input}
-            value={this.state.name}
-            onChange={event => this.setState({ id: event.target.value })}
-            margin="normal"
-          />
-          <Link to={ "victim/" + this.state.id } >
-            <Button
-              type="submit"
-              variant="outlined"
-              color="primary"
-              className={classes.input}
-            >
-              Otsi
-            </Button>
-          </Link>
-        </Paper>
-      </Layout>
-    );
-  }
+        return (
+            <Layout>
+                <Paper className={classes.paper}>
+                    <TextField
+                        id="id-field"
+                        label="ID"
+                        className={classes.input}
+                        value={this.state.name}
+                        onChange={event => this.setState({id: event.target.value})}
+                        margin="normal"
+                    />
+                    <Link to={"victim/" + this.state.id}>
+                        <Button
+                            type="submit"
+                            variant="outlined"
+                            color="primary"
+                            className={classes.input}
+                        >
+                            Otsi
+                        </Button>
+                    </Link>
+
+                </Paper>
+                <Paper className={classes.paper}>
+                    <Link to={"newUser/"}>
+                    <Button
+                        variant="raised"
+                        color="primary"
+                        >
+                        Uus isik
+                    </Button>
+                    </Link>
+
+                </Paper>
+
+            </Layout>
+        );
+    }
 }
 
 export default withRoot(withStyles(styles)(Overview));
