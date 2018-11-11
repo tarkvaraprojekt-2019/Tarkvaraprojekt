494430c96d75b808dd600bff8289310304379421
TornOne <ott.adermann@gmail.com>
2018-11-11 21:03:05 +0200

    Creating and updating victims works with partial info
    Renamed export_csv

--- api/create_victim.php
+++ api/create_victim.php
@@ -5,10 +5,45 @@ require "verify_token.php";
 //Unpack post body
 $body = json_decode(file_get_contents("php://input"), true);
 
-//Insert all the data into the database
+$insert_fields = array();
+$insert_params = array();
+$params = array("first_name", "last_name", "national_id", "phone", "email");
+
+//Loop through all parameters to see if they were asked to be inserted
+foreach ($params as $param) {
+	if ($body[$param] !== "") {
+		$insert_fields[] = $param;
+		$insert_params[] = $body[$param];
+	}
+}
+
+//Add editing user and current date
+$insert_fields[] = "muutja";
+$insert_params[] = $username;
+$insert_fields[] = "muutmisaeg";
+$insert_params[] = date("Y-m-d");
+
+//At least some info has to exist about the victim
+$c = count($insert_fields);
+if ($c <= 2) {
+	http_response_code(400);
+	echo "Missing parameters";
+	exit();
+}
+
+//Construct the query
+$insert_query = "INSERT INTO clients SET ";
+for ($i = 0; $i < $c; $i++) {
+	if ($i != 0) {
+		$insert_query .= ", ";
+	}
+	$insert_query .= $insert_fields[$i] . "=?";
+}
+
+//Execute the query
 $db = get_db();
-$stmt = mysqli_prepare($db, "INSERT INTO clients (first_name, last_name, national_id, phone, email, muutja, muutmisaeg) VALUES (?, ?, ?, ?, ?, ?, ?)");
-mysqli_stmt_bind_param($stmt, "sssssss", $body["first_name"], $body["last_name"], $body["national_id"], $body["phone"], $body["email"], $username, date("Y-m-d"));
+$stmt = mysqli_prepare($db, $insert_query);
+mysqli_stmt_bind_param($stmt, str_repeat("s", $c), ...$insert_params);
 if (mysqli_stmt_execute($stmt)) {
 	echo mysqli_fetch_row(mysqli_query($db, "SELECT LAST_INSERT_ID()"))[0];
 }
--- api/update_victim.php
+++ api/update_victim.php
@@ -5,8 +5,45 @@ require "verify_token.php";
 //Unpack post body
 $body = json_decode(file_get_contents("php://input"), true);
 
-//Update all data in the database
+$update_fields = array();
+$update_params = array();
+$params = array("first_name", "last_name", "national_id", "phone", "email");
+
+//Loop through all parameters to see if they were asked to be updated
+foreach ($params as $param) {
+	if ($body[$param] !== "") {
+		$update_fields[] = $param;
+		$update_params[] = $body[$param];
+	}
+}
+
+//Add editing user and current date
+$update_fields[] = "muutja";
+$update_params[] = $username;
+$update_fields[] = "muutmisaeg";
+$update_params[] = date("Y-m-d");
+
+//At least some info has to exist about the victim
+$c = count($update_fields);
+if ($c <= 2) {
+	http_response_code(400);
+	echo "Missing parameters";
+	exit();
+}
+
+//Construct the query
+$update_query = "UPDATE clients SET ";
+for ($i = 0; $i < $c; $i++) {
+	if ($i != 0) {
+		$update_query .= ", ";
+	}
+	$update_query .= $update_fields[$i] . "=?";
+}
+$update_query .= " WHERE id=?";
+$update_params[] = $body["id"];
+
+//Execute the query
 $db = get_db();
-$stmt = mysqli_prepare($db, "UPDATE clients SET first_name=?, last_name=?, national_id=?, phone=?, email=?, muutja=?, muutmisaeg=? WHERE id=?");
-mysqli_stmt_bind_param($stmt, "sssssssi", $body["first_name"], $body["last_name"], $body["national_id"], $body["phone"], $body["email"], $username, date("Y-m-d"), $body["id"]);
+$stmt = mysqli_prepare($db, $update_query);
+mysqli_stmt_bind_param($stmt, str_repeat("s", $c + 1), ...$update_params);
 echo mysqli_stmt_execute($stmt);
