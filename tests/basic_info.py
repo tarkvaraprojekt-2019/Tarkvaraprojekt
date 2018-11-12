e1304c4beef6fa9066010cf2bc912511ca7f8f1d
Oskar Lahesoo <oskar@kodukas.eu>
2018-11-12 06:31:34 +0200

    Increased test timeout limit for CI #57

--- tests/basic_info.py
+++ tests/basic_info.py
@@ -16,22 +16,22 @@ try:
     inputPassword.send_keys("asdf")
     inputPassword.submit()
 
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "first_name")))
+    WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.ID, "first_name")))
 
     inputFirstName = driver.find_element_by_id("first_name")
     inputFirstName.send_keys("Pille")
     inputFirstName.submit()
 
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//td[contains(text(), 'PAGAN')]")))
+    WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.XPATH, "//td[contains(text(), 'PAGAN')]")))
 
     row = driver.find_element_by_xpath("//td[contains(text(), 'PAGAN')]")
     row.click()
 
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "national_id")))
+    WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.ID, "national_id")))
 
     national_ID = driver.find_element_by_id("national_id")
 
-    WebDriverWait(driver, 10).until(EC.text_to_be_present_in_element_value((By.ID, "national_id"), "4"))
+    WebDriverWait(driver, 120).until(EC.text_to_be_present_in_element_value((By.ID, "national_id"), "4"))
     assert national_ID.get_attribute("value") == "49901329491"
     print ("Basic info retrieval - Success!")
 
--- tests/modify_info.py
+++ tests/modify_info.py
@@ -16,18 +16,18 @@ try:
     inputPassword.send_keys("asdf")
     inputPassword.submit()
 
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "first_name")))
+    WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.ID, "first_name")))
 
     inputFirstName = driver.find_element_by_id("first_name")
     inputFirstName.send_keys("Pille")
     inputFirstName.submit()
 
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//td[contains(text(), 'PAGAN')]")))
+    WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.XPATH, "//td[contains(text(), 'PAGAN')]")))
 
     row = driver.find_element_by_xpath("//td[contains(text(), 'PAGAN')]")
     row.click()
 
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//span[contains(text(), 'MUUDA ISIKUANDMEID')]")))
+    WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.XPATH, "//span[contains(text(), 'MUUDA ISIKUANDMEID')]")))
 
     editButton = driver.find_element_by_xpath("//span[contains(text(), 'MUUDA ISIKUANDMEID')]")
     editButton.click()
@@ -48,20 +48,20 @@ try:
     inputPassword.send_keys("asdf")
     inputPassword.submit()
 
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "first_name")))
+    WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.ID, "first_name")))
 
     inputFirstName = driver.find_element_by_id("first_name")
     inputFirstName.send_keys("Pille")
     inputFirstName.submit()
 
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//td[contains(text(), 'PAGAN')]")))
+    WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.XPATH, "//td[contains(text(), 'PAGAN')]")))
 
     row = driver.find_element_by_xpath("//td[contains(text(), 'PAGAN')]")
     row.click()
 
     phone = driver.find_element_by_id("phone")
 
-    WebDriverWait(driver, 10).until(EC.text_to_be_present_in_element_value((By.ID, "phone"), "1"))
+    WebDriverWait(driver, 120).until(EC.text_to_be_present_in_element_value((By.ID, "phone"), "1"))
     assert phone.get_attribute("value") == "12345"
     print ("Modifying info - Success!")
 
