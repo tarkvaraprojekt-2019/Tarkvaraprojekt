0d714badab0846bfb16c957bc53255af9fedcc23
Oskar Lahesoo <oskar@kodukas.eu>
2018-11-20 14:24:44 +0200

    Updated install.sh

--- test_conf/install.sh
+++ test_conf/install.sh
@@ -13,14 +13,9 @@ sudo apt-get update && sudo apt-get install -y nodejs
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get install -y yarn
-cd /home
-sudo git clone https://github.com/tarkvaraprojekt-2019/Tarkvaraprojekt.git
-cd /frontend
-sudo yarn install
-sudo yarn build
-sudo cp -r public/ ../
-cd ../test_conf
-sudo cp testsite.conf /etc/apache2/sites-available
+cd /home && sudo git clone https://github.com/tarkvaraprojekt-2019/Tarkvaraprojekt.git
+cd /home/Tarkvaraprojekt/frontend && sudo yarn install && sudo yarn build && sudo cp -r public/ ../
+sudo cp /home/Tarkvaraprojekt/test_conf/testsite.conf /etc/apache2/sites-available
 sudo a2enmod rewrites
 sudo a2enmod headers
 sudo a2enmod deflate
