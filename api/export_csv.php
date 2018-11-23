9d0cc602a1a3091f9c0747e880ccfba217b6f4af
TornOne <ott.adermann@gmail.com>
2018-11-23 21:45:12 +0200

    CSV Export is mostly correct now

--- api/export_csv.php
+++ api/export_csv.php
@@ -8,6 +8,8 @@ if (!$is_admin) {
 	exit();
 }
 
+$db = get_db();
+
 //Construct where query
 $where_fields = array();
 $where_params = array();
@@ -20,49 +22,60 @@ if (isset($_GET["kuni"])) {
 	$where_params[] = $_GET["kuni"];
 }
 
-$where_query = " WHERE ";
+$where_query = "";
 $c = count($where_fields);
 for ($i = 0; $i < $c; $i++) {
-	if ($i == 1) {
-		$where_query .= " AND " . $where_fields[$i];
-	} else {
-		$where_query .= $where_fields[$i];
-	}
+	$where_query .= " AND " . $where_fields[$i];
 }
 
-//Construct final query
-$final_query = "SELECT * FROM incidents JOIN sessions ON incidents.id = incident_id"; //TODO: COALESCE(incidents.id, incident_id)
+//Construct incident query
+$incident_query = "SELECT incidents.id, piirkond, kliendi_nr, keel, vanus, puue, lapsed, rasedus, elukoht, fuusiline_vagivald, vaimne_vagivald, majanduslik_vagivald, seksuaalne_vagivald, inimkaubandus, teadmata_vagivald, partner_vagivallatseja, ekspartner_vagivallatseja, vanem_vagivallatseja, laps_vagivallatseja, sugulane_vagivallatseja, tookaaslane_vagivallatseja, muu_vagivallatseja, vagivallatseja_vanus, vagivallatseja_sugu, laps_ohver, vana_ohver, muu_ohver, politsei, rahastus FROM incidents, sessions WHERE sessions.incident_id=incidents.id {$where_query} GROUP BY incidents.id";
 
-$db = get_db();
 if ($c == 0) {
-	$query_res = mysqli_query($db, $final_query);
+	$query_res = mysqli_query($db, $incident_query);
 } else {
-	$stmt = mysqli_prepare($db, $final_query . $where_query);
+	$stmt = mysqli_prepare($db, $incident_query);
 	mysqli_stmt_bind_param($stmt, str_repeat("s", $c), ...$where_params);
 	mysqli_stmt_execute($stmt);
 	$query_res = mysqli_stmt_get_result($stmt);
 }
-$res = mysqli_fetch_all($query_res, MYSQLI_ASSOC);
+$incident_res = mysqli_fetch_all($query_res, MYSQLI_ASSOC);
 
-if (count($res) == 0) {
-	http_response_code(400);
-	echo "Andmed puuduvad";
-	exit();
-}
-
-//Construct csv
-$csv = "";
-//Title row
-foreach ($res[0] as $head => $value) {
-	$csv .= $head . "\t";
-}
-$csv .= "\n";
-//Other rows
-foreach ($res as $line) {
-	foreach ($line as $value) {
-		$csv .= $value . "\t";
+//Construct CSV line by line
+$csv = "Tugikeskuse piirkond	Kliendi nr	Kuupäev	Korduvus	Juhtumi lühikirjeldus	Suhtluskeel	Vanus	Puue	Alaealiste laste arv	Rasedus	Elukoht	Füüsiline vägivald	Vaimne vägivald	Majanduslik vägivald	Seksuaalne vägivald	Inimkaubandus	Teadmata vägivald	Vägivallatseja partner	Vägivallatseja ekspartner	Vägivallatseja isa/ema	Vägivallatseja poeg/tütar	Vägivallatseja sugulane/hõimlane	Vägivallatseja töö- või õpingukaaslane	Vägivallatseja muu	Vägivallatseja vanus	Vägivallatseja sugu	Lisaohver - Alaealine laps	Lisaohver - Eakas (üle 65)	Lisaohver - Muu	Kas on pöördunud politseisse	Kas nõustamine toimus sidevahendite abil	Kriisinõustamine	Kriisinõustamise aeg	Juhtumipõhine nõustamine	Võrgustikutöö	Psühholoogiline nõustamine, psühhoteraapia	Juriidiline nõustamine	Tegevused lapsega	Tugiteenused (transport, toidu- ja riideabi jm)	Naise majutuspäevade arv	Kaasasolevate laste arv	Laste majutuspäevade arv	Juhtumipõhine ümarlaud	Suunatud MARACi	Perearst	EMO	Naistearst	Politsei	Prokuratuur	Riiklik ohvriabi	Lastekaitse	KOV sotsiaalabi	Kohus (tsiviilasjas)	Kohus (kriminaalasjas)	Haridusasutus	MTÜ-d	Sõbrad, sugulased	Rahastus	Märkused\n";
+foreach ($incident_res as $incident) {
+	//Construct session query
+	$session_query = "SELECT * FROM sessions WHERE incident_id={$incident["id"]}" . $where_query;
+	if ($c == 0) {
+		$query_res = mysqli_query($db, $session_query);
+	} else {
+		$stmt = mysqli_prepare($db, $session_query);
+		mysqli_stmt_bind_param($stmt, str_repeat("s", $c), ...$where_params);
+		mysqli_stmt_execute($stmt);
+		$query_res = mysqli_stmt_get_result($stmt);
 	}
-	$csv .= "\n";
+	$session_res = mysqli_fetch_all($query_res, MYSQLI_ASSOC);
+	
+	$session_count = count($session_res);
+	for ($i = 0; $i < $session_count; $i++) {
+		$session = $session_res[$i];
+		//First row of the incident
+		if ($i == 0) {
+			$csv .= $incident["piirkond"] . "\t" . $incident["kliendi_nr"] . "\t" . $session["kuupaev"] . "\t" . "Esmakordne" . "\t\"" . $session["kirjeldus"] . "\"\t" . $incident["keel"] . "\t" . $incident["vanus"] . "\t" . z($incident["puue"]) . "\t" . z($incident["lapsed"]) . "\t" . z($incident["rasedus"]) . "\t" . $incident["elukoht"] . "\t" . z($incident["fuusiline_vagivald"]) . "\t" . z($incident["vaimne_vagivald"]) . "\t" . z($incident["majanduslik_vagivald"]) . "\t" . z($incident["seksuaalne_vagivald"]) . "\t" . z($incident["inimkaubandus"]) . "\t" . z($incident["teadmata_vagivald"]) . "\t" . z($incident["partner_vagivallatseja"]) . "\t" . z($incident["ekspartner_vagivallatseja"]) . "\t" . z($incident["vanem_vagivallatseja"]) . "\t" . z($incident["laps_vagivallatseja"]) . "\t" . z($incident["sugulane_vagivallatseja"]) . "\t" . z($incident["tookaaslane_vagivallatseja"]) . "\t" . z($incident["muu_vagivallatseja"]) . "\t" . $incident["vagivallatseja_vanus"] . "\t" . $incident["vagivallatseja_sugu"] . "\t" . z($incident["laps_ohver"]) . "\t" . z($incident["vana_ohver"]) . "\t" . z($incident["muu_ohver"]) . "\t" . z($incident["politsei"]) . "\t" . z($session["sidevahendid"]) . "\t" . z($session["kriisinoustamine"]) . "\t" . $session["kriisinoustamise_aeg"] . "\t" . z($session["juhtuminoustamine"]) . "\t" . z($session["vorgustikutoo"]) . "\t" . z($session["psuhhonoustamine"]) . "\t" . z($session["juuranoustamine"]) . "\t" . z($session["tegevused_lapsega"]) . "\t" . z($session["tugiteenused"]) . "\t" . z($session["naise_majutus"]) . "\t" . z($session["laste_arv"]) . "\t" . z($session["laste_majutus"]) . "\t" . z($session["umarlaud"]) . "\t" . z($session["marac"]) . "\t" . z($session["perearst_kaasatud"]) . "\t" . z($session["emo_kaasatud"]) . "\t" . z($session["naistearst_kaasatud"]) . "\t" . z($session["politsei_kaasatud"]) . "\t" . z($session["prokuratuur_kaasatud"]) . "\t" . z($session["ohvriabi_kaasatud"]) . "\t" . z($session["lastekaitse_kaasatud"]) . "\t" . z($session["kov_kaasatud"]) . "\t" . z($session["tsiviilkohus_kaasatud"]) . "\t" . z($session["kriminaalkohus_kaasatud"]) . "\t" . z($session["haridusasutus_kaasatud"]) . "\t" . z($session["mtu_kaasatud"]) . "\t" . z($session["tuttavad_kaasatud"]) . "\t" . $incident["rahastus"] . "\t\"" . $session["markused"] . "\"\n";
+		}
+		//Following rows of one session each
+		else {
+			$csv .= $incident["piirkond"] . "\t" . $incident["kliendi_nr"] . "\t" . $session["kuupaev"] . "\t" . "Korduv" . "\t\"" . $session["kirjeldus"] . "\"\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t" . z($session["sidevahendid"]) . "\t" . z($session["kriisinoustamine"]) . "\t" . $session["kriisinoustamise_aeg"] . "\t" . z($session["juhtuminoustamine"]) . "\t" . z($session["vorgustikutoo"]) . "\t" . z($session["psuhhonoustamine"]) . "\t" . z($session["juuranoustamine"]) . "\t" . z($session["tegevused_lapsega"]) . "\t" . z($session["tugiteenused"]) . "\t" . z($session["naise_majutus"]) . "\t" . z($session["laste_arv"]) . "\t" . z($session["laste_majutus"]) . "\t" . z($session["umarlaud"]) . "\t" . z($session["marac"]) . "\t" . z($session["perearst_kaasatud"]) . "\t" . z($session["emo_kaasatud"]) . "\t" . z($session["naistearst_kaasatud"]) . "\t" . z($session["politsei_kaasatud"]) . "\t" . z($session["prokuratuur_kaasatud"]) . "\t" . z($session["ohvriabi_kaasatud"]) . "\t" . z($session["lastekaitse_kaasatud"]) . "\t" . z($session["kov_kaasatud"]) . "\t" . z($session["tsiviilkohus_kaasatud"]) . "\t" . z($session["kriminaalkohus_kaasatud"]) . "\t" . z($session["haridusasutus_kaasatud"]) . "\t" . z($session["mtu_kaasatud"]) . "\t" . z($session["tuttavad_kaasatud"]) . "\t\t\"" . $session["markused"] . "\"\n";
+		}
+	}
 }
 
 echo $csv;
+
+function z($string) {
+	if ($string === "0" || $string === "0.00") {
+		return "";
+	} else {
+		return $string;
+	}
+}
