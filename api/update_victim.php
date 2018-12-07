519f3bc8a36df5cbb0c1dea6e3a3bc8f20d1ebde
TornOne <ott.adermann@gmail.com>
2018-12-07 22:12:41 +0200

    Fixed password hash and username case insensitivity and victim edit bug

--- api/update_victim.php
+++ api/update_victim.php
@@ -10,7 +10,7 @@ $update_params = array();
 $params = array("first_name", "last_name", "national_id", "phone", "email");
 
 //Loop through all parameters to see if they were asked to be updated
-$c = 7; //Update field count if everything was to be updated
+$c = 5; //Update field count if everything was to be updated
 foreach ($params as $param) {
 	$update_fields[] = $param;
 	if ($body[$param] !== "") {
@@ -28,7 +28,7 @@ $update_fields[] = "muutmisaeg";
 $update_params[] = date("Y-m-d");
 
 //At least some info has to exist about the victim
-if ($c <= 2) {
+if ($c <= 0) {
 	http_response_code(400);
 	echo "Missing parameters";
 	exit();
@@ -36,7 +36,7 @@ if ($c <= 2) {
 
 //Construct the query
 $update_query = "UPDATE clients SET ";
-for ($i = 0; $i < $c; $i++) {
+for ($i = 0; $i < 7; $i++) {
 	if ($i != 0) {
 		$update_query .= ", ";
 	}
@@ -48,5 +48,5 @@ $update_params[] = $body["id"];
 //Execute the query
 $db = get_db();
 $stmt = mysqli_prepare($db, $update_query);
-mysqli_stmt_bind_param($stmt, str_repeat("s", $c + 1), ...$update_params);
+mysqli_stmt_bind_param($stmt, "sssssssi", ...$update_params);
 echo mysqli_stmt_execute($stmt);
