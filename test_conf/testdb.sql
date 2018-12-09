a9480aa4ac7f1741883e028e8a3c8225fd435a19
Oskar Lahesoo <oskar@kodukas.eu>
2018-12-10 00:57:43 +0200

    Trying out Cypress tests

--- test_conf/testdb.sql
+++ test_conf/testdb.sql
@@ -11,22 +11,25 @@ USE `test`;
 
 DELIMITER ;;
 
-CREATE PROCEDURE `create_user`(p_name VARCHAR(64), p_pass VARCHAR(255))
-REPLACE INTO users SET name = p_name, pass = p_pass, token = NULL;;
+CREATE PROCEDURE `create_user`(`p_name` VARCHAR(64), `p_pass` VARCHAR(255))
+INSERT INTO users SET name = p_name, pass = p_pass, token = NULL;;
 
-CREATE PROCEDURE `delete_user`(p_name VARCHAR(64))
+CREATE PROCEDURE `delete_user`(`p_name` VARCHAR(64))
 DELETE FROM users WHERE name = p_name;;
 
-CREATE PROCEDURE `get_user`(p_name VARCHAR(64), OUT p_pass VARCHAR(255), OUT p_is_admin BOOL, OUT p_token VARCHAR(32))
+CREATE PROCEDURE `get_user`(`p_name` VARCHAR(64), OUT `p_pass` VARCHAR(255), OUT `p_is_admin` BOOL, OUT `p_token` VARCHAR(32))
 SELECT pass, is_admin, token INTO p_pass, p_is_admin, p_token FROM users WHERE name = p_name;;
 
-CREATE PROCEDURE `log_user_out`(p_name VARCHAR(64))
+CREATE PROCEDURE `log_user_out`(`p_name` VARCHAR(64))
 UPDATE users SET token = NULL WHERE name = p_name;;
 
-CREATE PROCEDURE `replace_user_token`(p_name VARCHAR(64), p_token VARCHAR(32))
+CREATE PROCEDURE `replace_user_token`(`p_name` VARCHAR(64), `p_token` VARCHAR(32))
 UPDATE users SET token = p_token WHERE name = p_name;;
 
-CREATE PROCEDURE `set_user_admin`(p_name VARCHAR(64), p_admin BOOL)
+CREATE PROCEDURE `set_pass`(`p_name` VARCHAR(64), `p_pass` VARCHAR(255))
+UPDATE users SET pass = p_pass, token = NULL WHERE name = p_name;;
+
+CREATE PROCEDURE `set_user_admin`(`p_name` VARCHAR(64), `p_admin` BOOL)
 UPDATE users SET is_admin = p_admin WHERE name = p_name;;
 
 CREATE EVENT `reset_tokens` ON SCHEDULE EVERY 1 WEEK STARTS '2018-10-27 23:59:59' ON COMPLETION NOT PRESERVE ENABLE DO UPDATE users SET token = NULL;;
@@ -47,13 +50,14 @@ CREATE TABLE `clients` (
   UNIQUE KEY `national_id` (`national_id`),
   UNIQUE KEY `phone` (`phone`),
   UNIQUE KEY `email` (`email`),
-  KEY `clients_first_name_index` (`first_name`),
-  KEY `clients_last_name_index` (`last_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  FULLTEXT KEY `clients_first_name_index` (`first_name`),
+  FULLTEXT KEY `clients_last_name_index` (`last_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 TRUNCATE `clients`;
 INSERT INTO `clients` (`id`, `first_name`, `last_name`, `national_id`, `phone`, `email`, `muutja`, `muutmisaeg`) VALUES
-(2018171,	'Pille',	'PAGAN',	49901329491,	546,	'ab@c.d',	'asdf',	'2018-11-06');
+(2018171,	'Pille',	'PAGAN',	47102042742,	3725074194,	'pallamaali@gmail.com',	'asdf',	'2018-11-06'),
+(2018191,	'Mari',	'f',	NULL,	222555,	NULL,	'alo',	'2018-12-07');
 
 DROP TABLE IF EXISTS `incidents`;
 CREATE TABLE `incidents` (
@@ -91,12 +95,27 @@ CREATE TABLE `incidents` (
   PRIMARY KEY (`id`),
   KEY `kliendi_nr` (`kliendi_nr`),
   CONSTRAINT `incidents_ibfk_1` FOREIGN KEY (`kliendi_nr`) REFERENCES `clients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 TRUNCATE `incidents`;
 INSERT INTO `incidents` (`id`, `piirkond`, `kliendi_nr`, `keel`, `vanus`, `puue`, `lapsed`, `rasedus`, `elukoht`, `fuusiline_vagivald`, `vaimne_vagivald`, `majanduslik_vagivald`, `seksuaalne_vagivald`, `inimkaubandus`, `teadmata_vagivald`, `partner_vagivallatseja`, `ekspartner_vagivallatseja`, `vanem_vagivallatseja`, `laps_vagivallatseja`, `sugulane_vagivallatseja`, `tookaaslane_vagivallatseja`, `muu_vagivallatseja`, `vagivallatseja_vanus`, `vagivallatseja_sugu`, `laps_ohver`, `vana_ohver`, `muu_ohver`, `politsei`, `rahastus`, `muutja`, `muutmisaeg`) VALUES
-(520,	'Tartumaa',	2018171,	'eesti',	'25-49',	0,	2,	0,	'Tartumaa',	0,	1,	0,	0,	0,	0,	0,	1,	0,	0,	0,	0,	0,	'teadmata',	'teadmata',	1,	0,	0,	0,	'NTK rahastus',	'asdf',	'2018-11-06'),
-(521,	'Tartumaa',	2018171,	'eesti',	'25-49',	0,	2,	0,	'Tartumaa',	0,	1,	0,	0,	0,	0,	0,	1,	0,	0,	0,	0,	0,	'teadmata',	'teadmata',	1,	0,	0,	0,	'NTK rahastus',	'asdf',	'2018-11-06');
+(521,	'teadmata',	2018191,	'teadmata',	'teadmata',	0,	0,	0,	'teadmata',	0,	1,	1,	0,	0,	0,	1,	1,	1,	0,	1,	0,	0,	'teadmata',	'teadmata',	0,	0,	0,	0,	'Muu rahastus',	'alo',	'2018-12-08'),
+(522,	'teadmata',	2018191,	'teadmata',	'teadmata',	0,	0,	0,	'teadmata',	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	'teadmata',	'teadmata',	0,	0,	0,	0,	'Muu rahastus',	'alo',	'2018-11-23'),
+(523,	'teadmata',	2018191,	'teadmata',	'teadmata',	0,	0,	0,	'teadmata',	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	'teadmata',	'teadmata',	0,	0,	0,	0,	'Muu rahastus',	'alo',	'2018-11-23'),
+(524,	'teadmata',	2018191,	'teadmata',	'teadmata',	0,	0,	0,	'teadmata',	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	'teadmata',	'teadmata',	0,	0,	0,	0,	'Muu rahastus',	'alo',	'2018-11-23'),
+(536,	'teadmata',	2018191,	'teadmata',	'teadmata',	0,	0,	0,	'teadmata',	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	'teadmata',	'teadmata',	0,	0,	0,	0,	'Muu rahastus',	'alo',	'2018-12-07'),
+(537,	'teadmata',	2018191,	'teadmata',	'teadmata',	0,	-3,	0,	'teadmata',	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	'teadmata',	'teadmata',	0,	0,	0,	0,	'Muu rahastus',	'alo',	'2018-12-07'),
+(538,	'teadmata',	2018191,	'teadmata',	'teadmata',	0,	0,	0,	'teadmata',	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	'teadmata',	'teadmata',	0,	0,	0,	0,	'Muu rahastus',	'alo',	'2018-12-07'),
+(539,	'teadmata',	2018191,	'teadmata',	'teadmata',	0,	-1,	0,	'teadmata',	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	'teadmata',	'teadmata',	0,	0,	0,	0,	'Muu rahastus',	'alo',	'2018-12-07'),
+(540,	'teadmata',	2018191,	'teadmata',	'teadmata',	0,	-1,	0,	'teadmata',	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	'teadmata',	'teadmata',	0,	0,	0,	0,	'Muu rahastus',	'alo',	'2018-12-07'),
+(541,	'teadmata',	2018191,	'teadmata',	'teadmata',	0,	0,	0,	'teadmata',	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	'teadmata',	'teadmata',	0,	0,	0,	0,	'Muu rahastus',	'alo',	'2018-12-07'),
+(542,	'teadmata',	2018191,	'teadmata',	'teadmata',	0,	0,	0,	'teadmata',	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	'teadmata',	'teadmata',	0,	0,	0,	0,	'Muu rahastus',	'alo',	'2018-12-07'),
+(543,	'teadmata',	2018191,	'teadmata',	'teadmata',	0,	0,	0,	'teadmata',	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	'teadmata',	'teadmata',	0,	0,	0,	0,	'Muu rahastus',	'alo',	'2018-12-07'),
+(544,	'teadmata',	2018191,	'teadmata',	'teadmata',	0,	127,	0,	'teadmata',	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	'teadmata',	'teadmata',	0,	0,	0,	0,	'Muu rahastus',	'alo',	'2018-12-07'),
+(545,	'teadmata',	2018191,	'teadmata',	'teadmata',	0,	127,	0,	'teadmata',	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	'teadmata',	'teadmata',	0,	0,	0,	0,	'Muu rahastus',	'alo',	'2018-12-07'),
+(546,	'teadmata',	2018191,	'teadmata',	'teadmata',	0,	127,	0,	'teadmata',	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	'teadmata',	'teadmata',	0,	0,	0,	0,	'Muu rahastus',	'alo',	'2018-12-07'),
+(547,	'teadmata',	2018191,	'teadmata',	'teadmata',	0,	10,	0,	'teadmata',	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	'teadmata',	'teadmata',	0,	0,	0,	0,	'Muu rahastus',	'alo',	'2018-12-07'),
+(548,	'teadmata',	2018191,	'teadmata',	'teadmata',	0,	6,	0,	'teadmata',	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	'teadmata',	'teadmata',	0,	0,	0,	0,	'Muu rahastus',	'alo',	'2018-12-07');
 
 DROP TABLE IF EXISTS `sessions`;
 CREATE TABLE `sessions` (
@@ -137,24 +156,28 @@ CREATE TABLE `sessions` (
   PRIMARY KEY (`id`),
   KEY `incident_id` (`incident_id`),
   CONSTRAINT `sessions_ibfk_1` FOREIGN KEY (`incident_id`) REFERENCES `incidents` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 TRUNCATE `sessions`;
 INSERT INTO `sessions` (`id`, `incident_id`, `kuupaev`, `kirjeldus`, `sidevahendid`, `kriisinoustamine`, `kriisinoustamise_aeg`, `juhtuminoustamine`, `vorgustikutoo`, `psuhhonoustamine`, `juuranoustamine`, `tegevused_lapsega`, `tugiteenused`, `naise_majutus`, `laste_arv`, `laste_majutus`, `umarlaud`, `marac`, `perearst_kaasatud`, `emo_kaasatud`, `naistearst_kaasatud`, `politsei_kaasatud`, `prokuratuur_kaasatud`, `ohvriabi_kaasatud`, `lastekaitse_kaasatud`, `kov_kaasatud`, `tsiviilkohus_kaasatud`, `kriminaalkohus_kaasatud`, `haridusasutus_kaasatud`, `mtu_kaasatud`, `tuttavad_kaasatud`, `markused`, `muutja`, `muutmisaeg`) VALUES
-(4100,	521,	'2018-11-06',	'KRIISINÕUSTAMINE',	0,	1.00,	'teadmata',	1.00,	0.00,	0.00,	0.00,	1.00,	0.00,	0,	0,	0,	0,	0,	0,	0,	1,	0,	0,	0,	0,	0,	1,	0,	0,	0,	1,	'TEGELEME',	'asdf',	'2018-11-06'),
-(4101,	521,	'2018-11-06',	'JUHTUMIPLAANI ARUTELU',	0,	0.00,	'teadmata',	1.00,	0.00,	0.00,	0.00,	0.00,	0.00,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	'ARUTATI TEGEVUSPLAANI',	'asdf',	'2018-11-06');
+(4104,	524,	'2018-11-23',	'',	0,	0.00,	'teadmata',	0.00,	0.00,	0.00,	0.00,	0.00,	0.00,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	'',	'alo',	'2018-11-23'),
+(4105,	524,	'2018-11-23',	'',	0,	0.00,	'teadmata',	0.00,	0.00,	0.00,	0.00,	0.00,	0.00,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	'',	'alo',	'2018-11-23'),
+(4152,	523,	'2018-12-07',	'',	0,	0.00,	'teadmata',	0.00,	0.00,	0.00,	0.00,	0.00,	0.00,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	'',	'alo',	'2018-12-07'),
+(4153,	523,	'2018-12-07',	'',	0,	0.00,	'teadmata',	0.00,	0.00,	0.00,	0.00,	0.00,	0.00,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	'',	'alo',	'2018-12-07'),
+(4154,	548,	'0000-00-00',	'',	0,	0.00,	'teadmata',	0.00,	0.00,	0.00,	0.00,	0.00,	0.00,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	'',	'alo',	'2018-12-07'),
+(4155,	521,	'2018-12-07',	'',	0,	20.00,	'teadmata',	0.00,	55.00,	0.00,	1.00,	2.00,	0.00,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	'',	'alo',	'2018-12-08');
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `name` varchar(64) NOT NULL,
-  `pass` varchar(255) NOT NULL,
+  `name` varchar(64) COLLATE utf8_bin NOT NULL,
+  `pass` varchar(255) COLLATE utf8_bin NOT NULL,
   `is_admin` tinyint(1) NOT NULL DEFAULT 0,
-  `token` varchar(32) DEFAULT NULL,
+  `token` varchar(32) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 TRUNCATE `users`;
 INSERT INTO `users` (`name`, `pass`, `is_admin`, `token`) VALUES
-('asdf',	'$2y$10$7zKYAO7zCfDpp2dNOBu8Lei/woPTSa9W6BFrr5rRFCAKwoHp9Y1lS',	0,	'TsHReqQPZUKOf0hPfoYVlDZ3DIFbeBA/');
+('asdf',	'$2y$10$h4fekCjYORxzCaiel27mQ.dljWwShrLzMHJeWhOjxV.C7Mq.TMl3W',	1,	'pjJdiqaHt8hlL5HOLMUjndv674X+NKGR');
 
--- 2018-11-11 17:41:58
+-- 2018-12-09 20:02:19
