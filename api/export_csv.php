5ff472b5e2003dca3f8d125762613e8d06ea3d17
aloaas <aloaas@gmail.com>
2019-01-28 14:42:13 +0200

    [Fix] Kõige esimene sessioon kliendi kohta ainult esmakordne

--- api/export_csv.php
+++ api/export_csv.php
@@ -46,6 +46,8 @@ $csv = "Tugikeskuse piirkond	Kliendi nr	Kuupäev	Korduvus	Juhtumi lühikirjeldus
 foreach ($incident_res as $incident) {
 	//Construct session query
 	$session_query = "SELECT * FROM sessions WHERE incident_id={$incident["id"]}" . $where_query;
+	$incidents_query = "SELECT MIN(id) FROM incidents WHERE kliendi_nr={$incident["kliendi_nr"]}";
+	
 	if ($c == 0) {
 		$query_res = mysqli_query($db, $session_query);
 	} else {
@@ -56,11 +58,28 @@ foreach ($incident_res as $incident) {
 	}
 	$session_res = mysqli_fetch_all($query_res, MYSQLI_ASSOC);
 	
+	$query_inc_res = mysqli_query($db, $incidents_query);
+	$row1 = null;
+	$value1 = false;
+	if (mysqli_num_rows($query_inc_res) == 1) {
+		$row = mysqli_fetch_assoc($query_inc_res);
+		$value = $row["MIN(id)"] == $incident["id"];
+		if ($value) {
+				$first_session_query = "SELECT MIN(id) FROM sessions WHERE incident_id={$row["MIN(id)"]}";
+				$query_session_res = mysqli_query($db, $first_session_query);
+				if (mysqli_num_rows($query_session_res) == 1) {
+					$row1 = mysqli_fetch_assoc($query_session_res);
+			}	
+		}
+	}
 	$session_count = count($session_res);
 	for ($i = 0; $i < $session_count; $i++) {
 		$session = $session_res[$i];
+		if (!empty($row1)) {
+			$value1 = $row1["MIN(id)"] == $session["id"];
+		}
 		//First row of the incident
-		if ($i == 0) {
+		if ($value1) {
 			$csv .= $incident["piirkond"] . "\t" . $incident["kliendi_nr"] . "\t" . $session["kuupaev"] . "\t" . "Esmakordne" . "\t\"" . $session["kirjeldus"] . "\"\t" . $incident["keel"] . "\t" . $incident["vanus"] . "\t" . z($incident["puue"]) . "\t" . z($incident["lapsed"]) . "\t" . z($incident["rasedus"]) . "\t" . $incident["elukoht"] . "\t" . z($incident["fuusiline_vagivald"]) . "\t" . z($incident["vaimne_vagivald"]) . "\t" . z($incident["majanduslik_vagivald"]) . "\t" . z($incident["seksuaalne_vagivald"]) . "\t" . z($incident["inimkaubandus"]) . "\t" . z($incident["teadmata_vagivald"]) . "\t" . z($incident["partner_vagivallatseja"]) . "\t" . z($incident["ekspartner_vagivallatseja"]) . "\t" . z($incident["vanem_vagivallatseja"]) . "\t" . z($incident["laps_vagivallatseja"]) . "\t" . z($incident["sugulane_vagivallatseja"]) . "\t" . z($incident["tookaaslane_vagivallatseja"]) . "\t" . z($incident["muu_vagivallatseja"]) . "\t" . $incident["vagivallatseja_vanus"] . "\t" . $incident["vagivallatseja_sugu"] . "\t" . z($incident["laps_ohver"]) . "\t" . z($incident["vana_ohver"]) . "\t" . z($incident["muu_ohver"]) . "\t" . z($incident["politsei"]) . "\t" . z($session["sidevahendid"]) . "\t" . z($session["kriisinoustamine"]) . "\t" . $session["kriisinoustamise_aeg"] . "\t" . z($session["juhtuminoustamine"]) . "\t" . z($session["vorgustikutoo"]) . "\t" . z($session["psuhhonoustamine"]) . "\t" . z($session["juuranoustamine"]) . "\t" . z($session["tegevused_lapsega"]) . "\t" . z($session["tugiteenused"]) . "\t" . z($session["naise_majutus"]) . "\t" . z($session["laste_arv"]) . "\t" . z($session["laste_majutus"]) . "\t" . z($session["umarlaud"]) . "\t" . z($session["marac"]) . "\t" . z($session["perearst_kaasatud"]) . "\t" . z($session["emo_kaasatud"]) . "\t" . z($session["naistearst_kaasatud"]) . "\t" . z($session["politsei_kaasatud"]) . "\t" . z($session["prokuratuur_kaasatud"]) . "\t" . z($session["ohvriabi_kaasatud"]) . "\t" . z($session["lastekaitse_kaasatud"]) . "\t" . z($session["kov_kaasatud"]) . "\t" . z($session["tsiviilkohus_kaasatud"]) . "\t" . z($session["kriminaalkohus_kaasatud"]) . "\t" . z($session["haridusasutus_kaasatud"]) . "\t" . z($session["mtu_kaasatud"]) . "\t" . z($session["tuttavad_kaasatud"]) . "\t" . $incident["rahastus"] . "\t\"" . $session["markused"] . "\"\n";
 		}
 		//Following rows of one session each
